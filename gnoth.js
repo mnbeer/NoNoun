bb3caeb5e6086619ce98ecad105dfcade252a57e
Matthew Beer <matthew@rocketsocks.com>
2018-03-15 23:52:55 -0700

    Interim check-in: begin folding in chrome storage to save user settings.

--- gnoth.js
+++ gnoth.js
@@ -1,6 +1,69 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var GoGnothButton = document.getElementById('GoGnothButton');
-    GoGnothButton.addEventListener('click', function () {
+(function () {
+
+    //var settings;
+    var key = 'lastSettings';
+
+    var fillDefaultSettings = function (settings) {
+        settings.he = settings.he || "o";
+        settings.she = settings.she || "o";
+        settings.him = settings.him || "om";
+        settings.her = settings.her || "om";
+        settings.his = settings.his || "oz";
+        settings.hers = settings.his || "her";
+        return settings;
+    }
+
+    // get user's last choices from storage
+    var getSettings = new Promise(function (resolve, reject) {
+        if (chrome.storage) {
+            chrome.storage.sync.get([key], function (result) {
+                console.log('Value currently is ' + JSON.stringify(result));
+                console.log('Value currently is ' + result.key);
+                // fill in with default values if previous user choices
+                // do not exist
+                settings = fillDefaultSettings(result || {});
+                resolve(result);
+            });
+        }
+        else {
+            settings = fillDefaultSettings({});
+            resolve(settings);
+        }
+    });
+
+    //var getSettings = function (callback) {
+    //    chrome.storage.sync.get([key], function (result) {
+    //        console.log('Value currently is ' + JSON.stringify(result));
+    //        console.log('Value currently is ' + result.key);
+    //        settings = fillDefaultSettings(result);
+    //        if (callback) {
+    //            callback(result);
+    //        }
+    //    });
+    //};
+
+    // Fill text boxes with initial values. User can then
+    // change those values as desired.
+    var fillSettings = function(settings) {
+        document.getElementById('HeReplacement').value = settings.he;
+        document.getElementById('SheReplacement').value = settings.she;
+        document.getElementById('HimReplacement').value = settings.him;
+        document.getElementById('HerReplacement').value = settings.her;
+        document.getElementById('HisReplacement').value = settings.his;
+        document.getElementById('HersReplacement').value = settings.hers;
+    }
+
+    // store the user's choices for next time
+    var setSettings = function (newSettings) {
+        if (chrome.storage) {
+            chrome.storage.sync.set({ key: newSettings }, function () {
+                console.log('Value is set to ' + JSON.stringify(newSettings));
+            });
+        }
+    };
+
+    // Return all the user's choices in one object
+    var getUserValues = function () {
         var heReplacement = document.getElementById('HeReplacement').value;
         var sheReplacement = document.getElementById('SheReplacement').value;
         var himReplacement = document.getElementById('HimReplacement').value;
@@ -11,64 +74,32 @@ document.addEventListener('DOMContentLoaded', function () {
             he: heReplacement, him: himReplacement, his: hisReplacement,
             she: heReplacement, her: himReplacement, hers: hisReplacement,
         };
+        return g;
+    }
 
-        chrome.tabs.executeScript(null, {
-            code: 'var gnoth = {}; gnoth.g = ' + JSON.stringify(g) + ';'
-        }, function () {
-            chrome.tabs.executeScript(null, { file: 'gnoth-inject.js' });
-        });
-
+    // Add a click event to the "Go" button
+    document.addEventListener('DOMContentLoaded', function () {
+        var GoGnothButton = document.getElementById('GoGnothButton');
+        GoGnothButton.addEventListener('click', function () {
+            var g = getUserValues();   // get user's choices
+            setSettings(g);            // save user choices for next time
+            // Now go do it!!
+            chrome.tabs.executeScript(null, {
+                code: 'var gnoth = {}; gnoth.g = ' + JSON.stringify(g) + ';'
+            }, function () {
+                chrome.tabs.executeScript(null, { file: 'gnoth-inject.js' });
+            });
+        }, false);
     }, false);
-}, false);
 
 
-//function gnothReplace(g)
-//{
-//    var elements = document.getElementsByTagName('*');
-
-//    for (var i = 0; i < elements.length; i++) {
-//        var element = elements[i];
-
-//        for (var j = 0; j < element.childNodes.length; j++) {
-//            var node = element.childNodes[j];
-
-//            if (node.nodeType === 3) {
-//                var text = node.nodeValue;
-//                var replacedText = gnothReplaceOne(g, text);
-
-//                if (replacedText !== text) {
-//                    element.replaceChild(document.createTextNode(replacedText), node);
-//                }
-//            }
-//        }
-//    }
-//}
-
-//function gnothReplaceOne(g, text) {
-//    var replacedText = text;
-//    if (g.he) {
-//        var regex = new RegExp("\b" + g.he + "\b", "ig");
-//        replacedText = text.replace('\bhe\b/gi', g.he);
-//    }
-//    return replacedText;
-//}
-
-//var elements = document.getElementsByTagName('*');
-
-//for (var i = 0; i < elements.length; i++) {
-//    var element = elements[i];
-
-//    for (var j = 0; j < element.childNodes.length; j++) {
-//        var node = element.childNodes[j];
-
-//        if (node.nodeType === 3) {
-//            var text = node.nodeValue;
-//            var replacedText = text.replace(/[word or phrase to replace here]/gi, '[new word or phrase]');
-
-//            if (replacedText !== text) {
-//                element.replaceChild(document.createTextNode(replacedText), node);
-//            }
-//        }
-//    }
-//}
+    // Get user's last settings from storage - if they exist. Then
+    // fill those values - or defaults, if the values do not exist -
+    // into the text boxes to show user.
+    getSettings
+        .then(function (settings) {
+            fillSettings(settings);
+        })
+ 
+})();
 
