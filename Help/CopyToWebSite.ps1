81bcde0f48703608fc0b4878ba220b893cad523a
Matthew Beer <matthew@rocketsocks.com>
2018-04-06 19:56:17 -0700

    Finish up 1st draft of help

---
+++ Help/CopyToWebSite.ps1
@@ -0,0 +1,3 @@
+Copy-Item "C:\Users\Matthew\Documents\Code\NoNoun\Help\Index.html" -Destination "C:\Users\Matthew\Documents\Code\www.rocketsocks.com\htdocs\"
+Copy-Item "C:\Users\Matthew\Documents\Code\NoNoun\Help\css\nonoun.css" -Destination "C:\Users\Matthew\Documents\Code\www.rocketsocks.com\htdocs\css\"
+Copy-Item "C:\Users\Matthew\Documents\Code\NoNoun\Help\images\*.png" -Destination "C:\Users\Matthew\Documents\Code\www.rocketsocks.com\htdocs\images\"
