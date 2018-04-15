ffc78a3eec6d021b4830b7079b6285b44216c15e
Matthew Beer <matthew@rocketsocks.com>
2018-04-15 08:18:22 -0700

    Add google analytics

--- Help/CopyToWebSite.ps1
+++ Help/CopyToWebSite.ps1
@@ -1,3 +1,3 @@
-Copy-Item "C:\Users\Matthew\Documents\Code\NoNoun\Help\Index.html" -Destination "C:\Users\Matthew\Documents\Code\www.rocketsocks.com\htdocs\"
-Copy-Item "C:\Users\Matthew\Documents\Code\NoNoun\Help\css\nonoun.css" -Destination "C:\Users\Matthew\Documents\Code\www.rocketsocks.com\htdocs\css\"
-Copy-Item "C:\Users\Matthew\Documents\Code\NoNoun\Help\images\*.png" -Destination "C:\Users\Matthew\Documents\Code\www.rocketsocks.com\htdocs\images\"
+Copy-Item "C:\Users\Matthew\Documents\Code\NoNoun\Help\Index.html" -Destination "C:\Users\Matthew\Documents\Code\www.rocketsocks.com\htdocs\nonoun"
+Copy-Item "C:\Users\Matthew\Documents\Code\NoNoun\Help\css\nonoun.css" -Destination "C:\Users\Matthew\Documents\Code\www.rocketsocks.com\htdocs\nonoun\css\"
+Copy-Item "C:\Users\Matthew\Documents\Code\NoNoun\Help\images\*.png" -Destination "C:\Users\Matthew\Documents\Code\www.rocketsocks.com\htdocs\nonoun\images\"
